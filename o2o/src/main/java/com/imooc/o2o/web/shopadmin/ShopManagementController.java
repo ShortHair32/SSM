8617cc02a22e7b2e86c29edb650dfc22913ba51d
ShortHair32 <1044919284@qq.com>
2019-04-04 19:31:56 +0800

    a

--- o2o/src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
+++ o2o/src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
@@ -33,6 +33,7 @@ import com.imooc.o2o.exceptions.ShopOperationException;
 import com.imooc.o2o.service.AreaService;
 import com.imooc.o2o.service.ShopCategoryService;
 import com.imooc.o2o.service.ShopService;
+import com.imooc.o2o.util.CodeUtil;
 import com.imooc.o2o.util.HttpServletRequestUtil;
 import com.imooc.o2o.util.ImageUtil;
 import com.imooc.o2o.util.PathUtil;
@@ -69,6 +70,11 @@ public class ShopManagementController {
 	@ResponseBody
 private Map<String,Object> registerShop(HttpServletRequest request){
 	Map<String, Object> modelMap=new HashMap<String, Object>();
+	if(!CodeUtil.checkVerifyCode(request)) {
+		modelMap.put("success",false);
+		modelMap.put("errMsg","输入了错误的验证码");
+		return modelMap;
+	}
 	//接受并转化相应参数
 	String shopStr=HttpServletRequestUtil.getString(request, "shopStr");
 	ObjectMapper mapper=new ObjectMapper();
--- o2o/src/main/webapp/resources/js/shop/shopoperation.js
+++ o2o/src/main/webapp/resources/js/shop/shopoperation.js
@@ -25,8 +25,8 @@ $(function(){
 			var shop={};
 			shop.shopName=$('#shop-name').val();
 			shop.shopAddr=$('#shop-addr').val();
-			shop.phone=$('shop-phone').val();
-			shop.shopDesc=$('shop-desc').val();
+			shop.phone=$('#shop-phone').val();
+			shop.shopDesc=$('#shop-desc').val();
 			shop.shopCategory={
 					shopCategoryId:$('#shop-category').find('option').not(function(){
 						return !this.selected;
@@ -41,25 +41,26 @@ $(function(){
 			var formData =new FormData();
 			formData.append('shopImg',shopImg);
 			formData.append('shopStr',JSON.stringify(shop));
-			var verfyCodeActual=$('#j_captcha').val();
-			if(!cerfyCodeActual){
+			var verifyCodeActual=$('#j_captcha').val();
+			if(!verifyCodeActual){
 				$.toast('请输入验证码！');
 				return;
 			}
-			formData.append('verfyCodeActual',verifyCodeActual);
+			formData.append("verifyCodeActual",verifyCodeActual);
 			$.ajax({
 				url:registerShopUrl,
 				type:'POST',
 				data:formData,
 				contentType:false,
-				proceesData:false,
+				processData:false,
 				cache:false,
 				success:function(data){
 					if(data.success){
-						$.toast('提交成功！');
-					}else{
-						$toast('提交失败！'+data.errMsg);
+						$.toast('提交成功！');}
+					else{
+						$.toast('提交失败！'+data.errMsg);
 					}
+					$('#captcha_img').click();
 				}
 			});
 		});
